24dd96aeb8c28fd01271b837498cf503d52b29bc
Gabriel Cerioni <gacerioni@gmail.com>
2021-03-30 22:35:22 -0300

    Create config.py

---
+++ api/config.py
@@ -0,0 +1,6 @@
+mongodb_config = dict(
+    mongodb_user = "",
+    mongodb_pwd = "",
+    mongodb_cluster_conn_str = "",
+    mongodb_tibia_db = "tibia",
+    mongodb_tibia_item_id_collection = "item_and_id")
