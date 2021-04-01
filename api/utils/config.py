53aaf3ed1a29322dce315ab5854859620b468905
Gabriel Cerioni <gacerioni@gmail.com>
2021-04-01 08:14:03 -0300

    Update config.py

--- api/utils/config.py
+++ api/utils/config.py
@@ -1,6 +1,6 @@
 mongodb_config = dict(
-    mongodb_user = "gabsthecreator",
-    mongodb_pwd = "137928ita",
+    mongodb_user = "",
+    mongodb_pwd = "",
     mongodb_cluster_conn_str = "cluster0.dkifc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     mongodb_tibia_db = "tibia",
     mongodb_tibia_item_id_collection = "item_and_id")
